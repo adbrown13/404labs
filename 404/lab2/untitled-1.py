1dde08d8e31f07f47459794ff30f0d3445578230
Ashley Fegan <adbrown@cs.ualberta.ca>
2015-01-20 09:39:13 -0700

    try

--- 404/lab2/untitled-1.py
+++
@@ -1,9 +0,0 @@
-import socket
-import sys
-try:
-    s=socket.socket(socket_AF_INET,socket.SOCKET_STREAM)
-except:
-    print('failed to create socket')
-    print('Error code: '+str(msg[0])+', Error message: '+str(msg[1]))
-    sys.exit()
-print('socket created')
